1c840de02ece273af99874faebfbdb05e4eb3019
Kobakat <54965702+Kobakat@users.noreply.github.com>
2020-03-03 20:44:38 -0500

    Test

--- Elevator.c
+++ Elevator.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node 
+//Linked List
+
+struct Node
 {
 	int data;
 	struct Node* next;
@@ -33,9 +35,24 @@ void append(struct Node** head, int new_data)
 
 	//Once at the end of the LL, add the new node
 	last->next = new_node;
-	
+
 	//New node's previous pointer points to the now second to last node.
 	new_node->previous = last;
+}
+
+void DeleteList(struct Node** head_ref)
+{
+	struct Node* current = *head_ref;
+	struct Node* next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head_ref = NULL;
 }
 
 void print_list(struct Node* n)
@@ -46,150 +63,148 @@ void print_list(struct Node* n)
 	}
 }
 
-/*void ChangeFloor(int start, int size, struct Node* head, int * list)
-{
-	for(int i = 0; i < size; i++)
-	{
-		int difference = *(list + i) - start;
-		
-		//decrease if change in floors is negative
-		if (difference < 0) 
-		{
-			for(int k = start; k > *(list + i); k--) 
-			{
-				printf("%d, ", floors[k-2]);
-			}
-			printf("Ding!\n");
-		}	
-	
-		//increase if change in floors is positive
-		else if (difference > 0)
-		{
-			for(int k = start; k < *(list + i); k++)
-			{
-				printf("%d, ", floors[k]);
-			}
-			printf("Ding!\n");
-		}
 
-		//If no net floor change occurs
-		else
-		{
-			printf("Oops! You're already on that floor!\n");
-		}
-		
-		start = *(list + i);
-	}	
-}*/
 
+
+
+
+
+//Project methods
 
 //Places the desired floors into a link list
-struct Node * AssignFloors(struct Node* head, int numOfFloors) 
+struct Node* AssignFloors(struct Node* head, int numOfFloors)
 {
 	struct Node* userList = NULL;
-	
-	for(int j = 0; j < numOfFloors; j++) 
+
+	for (int j = 0; j < numOfFloors; j++)
 	{
 		int chosenFloor = 0;
-		
-		printf("Enter floor number #%d\n", j+1);
+
+		printf("Enter floor number #%d\n", j + 1);
 		scanf("%d", &chosenFloor);
 
-		//incorrect floor chosen
-		if(chosenFloor > 15 || chosenFloor < 1) 
+		//bad input
+		if (chosenFloor > 15 || chosenFloor < 1)
 		{
 			printf("That is not a valid floor! Choose between 1-15.\n");
 			j--;
 		}
-		
-		//proper floor chosen
+
 		else
-		{	
+		{
 			struct Node* headref = head;
-			while (head->data != chosenFloor) 
+			while (head->data != chosenFloor)
 			{
 				head = head->next;
 			}
 			append(&userList, chosenFloor);
 			head = headref;
 		}
+
 	}
 
 	return userList;
 }
 
-struct Node* OrganizeFloors(struct Node* userList) 
+//Arranges the data contained by smallest to biggest
+struct Node* OrganizeFloors(struct Node* userList)
 {
+	//holds a reference to the original list
 	struct Node* headref = userList;
-	while (userList->next != NULL) 
-	{
-		if (userList->data > userList->next->data) 
-		{
-			int temp = userList->data;
-			userList->data = userList->next->data;
-			userList->next->data = temp;
 
+	while (userList->next != NULL)
+	{
+		//holds a temporary reference of a temporary list..
+		struct Node* temp = userList;
+		while (userList->next != NULL)
+		{
+			//Swaps elements
+			if (userList->data > userList->next->data)
+			{
+				int temp = userList->data;
+				userList->data = userList->next->data;
+				userList->next->data = temp;
+			}
 			userList = userList->next;
 		}
+		//Restore semi-temporary list
+		userList = temp;
+		userList = userList->next;
 	}
+
+	//Restore the original list with updated data
 	userList = headref;
+
+	DeleteList(&headref);
 
 	return userList;
 }
 
-int main() 
+//Returns the last data value from the refinedUser link list
+int ChangeFloor(struct Node* refinedUser)
+{
+	struct Node* headref = refinedUser;
+	while (refinedUser->next != NULL)
+	{
+		refinedUser = refinedUser->next;
+	}
+
+	int newFloor = refinedUser->data;
+	refinedUser = headref;
+
+	return newFloor;
+}
+
+
+
+
+
+
+
+int main()
 {
 	//Create empty doubly linked list
 	struct Node* floors = NULL;
 
 	//Add 15 nodes each with a floor to the list
-	for(int q = 1; q < 16; q++) 
+	for (int q = 1; q < 16; q++)
 	{
 		append(&floors, q);
 	}
 
-	
+
 	char again = 'y';
 	int currentFloor = floors->data;
-	
+
 	do
 	{
 		int numOfFloors = 0;
 
 		printf("You are currently on floor %d. The top floor is 15.\n", currentFloor);
-		printf("How many floors would you like to move between? ");
 
-		//Used to determine the size of the array
+		printf("How many floors would you like to move between? ");
 		scanf("%d", &numOfFloors);
 
-		//floorList points to an array of size numOfFloors
+		//Places desired floors into a list, then sorts them by size
 		struct Node* rawUser = AssignFloors(floors, numOfFloors);
-		printf("\nRaw: ");
-		print_list(rawUser);
 		struct Node* refinedUser = OrganizeFloors(rawUser);
-		printf("\nRefined: ");
+
+		printf("\nHere we go!\n");
 		print_list(refinedUser);
-		printf("\nHere we go!\n\n");
+		printf("Ding!\n\n");
 
-		//Text showing the changing of floors
+		currentFloor = ChangeFloor(refinedUser);
 
-		/*ChangeFloor(currentFloor, numOfFloors, floors, floorList);
 
-		currentFloor = *(floorList + numOfFloors - 1);
-
-		free(floorList);
-		floorList = NULL;
 
 		//ride again if input is 'y'
 		printf("Would you like to change floors? Enter y if yes or any other key if not.\n");
 
-
 		scanf("%s", &again);
 
+		printf("\n");
 
-		printf("\n");*/
+	} while (again == 'y');
 
-	} while(again == 'y'); 
-	
 	return 0;
 }
